e808043d6888a610420b31e825b3365edf559f04
matt-dalessandro <36138251+matt-dalessandro@users.noreply.github.com>
2018-10-20 16:52:56 -0400

    Update altFilter.py
    Minor change to maximize function where omega is returned instead of the index value.  Adds documentation to each function including a brief description, inputs required, and outputs

--- altFilter.py
+++ altFilter.py
@@ -1,14 +1,21 @@
 '''
 Altitude Filter
 By: Matthew D'Alessandro
+
+matplotlib must be installed 
 '''
 
-import csv
-import random
-import matplotlib.pyplot as plt
-import statistics as st
+import csv  #imports native csv library to access data
+import random  #imports native ranom library to genenrate random error
+import matplotlib.pyplot as plt  #library which generates plots
+import statistics as st #native library to calculate stats quickly
 
 def readCSV(filename):
+    '''
+    Description: Pulls the first column of data from a csv file.  The column must not have any titles
+    Inputs: Only the filename is required as a string in the form "filename.csv"
+    Outputs: Returns the information in a list where the first entry is in data[0][0] and the second entry is in data[0][1]
+    '''
     ifile = open(filename, "rU")
     reader = csv.reader(ifile, delimiter=";")
     rownum = 0
@@ -20,6 +27,11 @@ def readCSV(filename):
     return data
 
 def createError(data, timeData):
+    '''
+    Description: Adds +/- 10% error to every data point in a list as well as formatting the data into one list from the previous format from readCSV()
+    Inputs: Requires the altitude data and time data from the readCSV function
+    Outputs: Returns the true altitude, altitude, and time all in lists inside of a single list, these can be seperated by altTrue = info[0], alt = info[1], time = info[2]
+    '''
     altTrue = []
     alt = []
     time = []
@@ -34,21 +46,31 @@ def createError(data, timeData):
         rownum += 1
     return altTrue, alt, time
 
-def plot1(x,y1):
+def plot1(x,y1,title,xtit,ytit):
+    '''
+    Description: Generates a scatter plot from a given (x, y) dataset as a list and a title, x label, and y label as strings
+    Inputs: Requires x data and y data given each in a seperate list, followed by a title, x label, and y label as individual strings
+    Outputs: Does not return anything, instead generates a plot that halts the program until the plot is closed manually
+    '''
     fig = plt.figure()
     ax1 = fig.add_subplot(111)
-    ax1.scatter(x, y1, s=10, c='b', marker="s", label='Altitude with error')
-    ax1.set_xlabel("Omega")
-    ax1.set_ylabel("Error Reduction (%)")
-    ax1.set_title("Error Reduction from Filter")
+    ax1.scatter(x, y1, s=10, c='b', marker="s", label='Dataset 1')
+    ax1.set_xlabel(xtit)
+    ax1.set_ylabel(ytit)
+    ax1.set_title(title)
     plt.show()
 
-def plot2(time,y2,y1):
+def plot3(time, y1, y2, y3):
+    '''
+    Description: Generates a scatter plot from three given (x, y1), (x, y2), (x, y3) datasets, note the first y set will be printed to screen first
+    Inputs: Requires x data followed by the three y datasets all in lists
+    Outputs: Does not return anything, instead generates a plot that halts the program until the plot is closed manually
+    '''
     fig = plt.figure()
     ax1 = fig.add_subplot(111)
-    ax1.scatter(time, y1, s=10, c='b', marker="s", label='Altitude with error')
+    ax1.scatter(time, y1, s=10, c='b', marker="s", label='Altitude with Error')
     ax1.scatter(time, y2, s=10, c='r', marker="o", label='Filtered')
-
+    ax1.scatter(time, y3, s=5, c='g', marker="o", label='True Altitude from Simulation')
     plt.legend(loc='upper right');
     ax1.set_xlabel("Time (s)")
     ax1.set_ylabel("Altitude (m)")
@@ -56,6 +78,11 @@ def plot2(time,y2,y1):
     plt.show()
 
 def altFilter(alt, time, omega):
+    '''
+    Description: Filters out error by predicting the next step and weighting that prediction by a given omega value (0 -> 1)
+    Inputs: Requires altitude data and time dat given in lists as well as a value for omega in range [0,1]
+    Outputs: Returns a filtered list of altitude points
+    '''
     cp = 0
     cv = 0
     ca = 0
@@ -85,6 +112,11 @@ def altFilter(alt, time, omega):
     return filtered
 
 def avgError(l1, l2):
+    '''
+    Description: Returns a list of the delta between two lists on an entry to entry level
+    Inputs: Two lists where len(l1) < len(l2)
+    Outputs: A list of all deltas
+    '''
     l3 = []
     c = 0
     for i in l1:
@@ -93,6 +125,11 @@ def avgError(l1, l2):
     return l3
 
 def runTheFilter(omega):
+    '''
+    Description: Generates error, and then runs the filter for a given omega value and returns the error reduction percentage
+    Inputs: A value of omega in range [0,1]
+    Outputs: Error reduction value in percentage
+    '''
     info = createError(data, timeData)
     altTrue = info[0]
     alt = info[1]
@@ -100,11 +137,15 @@ def runTheFilter(omega):
     filtered = altFilter(alt, time, omega)
     error = st.mean(avgError(alt, altTrue))
     filterError = st.mean(avgError(filtered, altTrue))
-    errorReduction = (error-filterError)/error
-    #plot2(time, alt, filtered)
+    errorReduction = (error-filterError)/error * 100
     return errorReduction
 
 def plotTheFilter(omega):
+    '''
+    Description: Runs the filter a single time as well as generates a plot of the data
+    Inputs: Only requires a a value for omega in range [0,1]
+    Outputs: Returns a plot that halts program until manually closed and then returns the error reduction
+    '''
     info = createError(data, timeData)
     altTrue = info[0]
     alt = info[1]
@@ -113,22 +154,32 @@ def plotTheFilter(omega):
     error = st.mean(avgError(alt, altTrue))
     filterError = st.mean(avgError(filtered, altTrue))
     errorReduction = (error-filterError)/error
-    plot2(time, altTrue, filtered)
+    plot3(time, alt, filtered, altTrue)
     return errorReduction
 
 def optimize(points):
+    '''
+    Description: Runs the filter cycling omega from 0 to 1 given x number of points
+    Inputs: A positive integer value of points
+    Outputs: A list with column 1 containing the omega value and column 2 containing the error reduction for the given omega
+    '''
     omegaValue = []
     errorReduction = []
     for i in range(points):
         omega = i/points
-        #print(omega)
+        print(omega)
         error = runTheFilter(omega)
         omegaValue.append(omega)
         errorReduction.append(error)
-    plot1(omegaValue,errorReduction)
+    plot1(omegaValue, errorReduction, "Error Reduction by Omega", "Omega", "Error Reduction (%)")
     return(omegaValue, errorReduction)
 
 def maximize(info):
+    '''
+    Description: Returns the maximum error reduction as well as omega value when you pass it the optimize() function
+    Inputs: The optimize function
+    Outputs: Maximum erro reduction with associated omega value
+    '''
     maxValue = 0
     index = 0
     c = 0
@@ -141,35 +192,5 @@ def maximize(info):
 
 data = readCSV("altData.csv")
 timeData = readCSV("timeData.csv")
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
